c37aba218c01b9cd5a527a1a054d66d1dbdb1801
nehahebbar <nehahebbar@gmail.com>
2015-07-05 22:32:51 -0700

    set up front page links properly

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,22 +1,52 @@
 class ListingsController < ApplicationController
   def index
+    @listings = Listing.all
   end
 
   def new
+    @listing = Listing.new
   end
 
   def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      redirect_to listings_index_path
+    else
+      render "new"
+    end
   end
 
   def edit
+    @listing = set_listing
   end
 
   def update
+    @listing = set_listing
+    if @listing.update(listing_params)
+      redirect_to listing_path(@listing), notice: "Listing updated"
+    else
+      render "edit"
+    end
   end
 
   def show
+    @listing = set_listing
   end
 
   def destroy
+    @listing = set_listing
+    @listing.destroy
+    redirect_to listings_path
   end
-end
+
+  private
+
+  def set_listing
+    Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:listing).permit(:title, :description, :estimated_hours, :skill_ids => [])
+  end
+
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Skill.create(name: "Web Development")
+Skill.create(name: "Finance/Accounting")
+Skill.create(name: "Marketing/Social Media")
+Skill.create(name: "Strategy Consulting")
+Skill.create(name: "Data Analysis")
+
+
+Listing.create(title: "Earned Income Plan", description: "This project will save us $8,000, allowing us to support 5 projects and sites that allow local families to effectively manage the resources and grow food.", estimated_hours: 35)
+Listing.create(title: "Social Media Revamp", description: "We would like to rebrand ourselves through social media, to raise awareness and interest.", estimated_hours: 20)
+Listing.create(title: "Website Redesign", description: "We'd like to increase usage by redesigning the website to be more intuitive.", estimated_hours: 40)
+Listing.create(title: "Budgeting Plan", description: "This will help us save $10,000 by saving money and allocating it carefully.", estimated_hours: 15)
